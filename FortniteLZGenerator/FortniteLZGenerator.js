16af4e0a6405ea182a9c8cf3d64d90d6dbb77a55
Ledzeff <37081231+Ledzeff@users.noreply.github.com>
2018-04-05 20:47:59 -0600

    Update FortniteLZGenerator.js

--- FortniteLZGenerator/FortniteLZGenerator.js
+++ FortniteLZGenerator/FortniteLZGenerator.js
@@ -1,4 +1,25 @@
-const landingZones = ['Pleasant Park', 'Tilted Towers', 'Retail Row'];
+const landingZones = [
+	'Anarchy Acres', 
+	'Pleasant Park', 
+	'Tilted Towers', 
+	'Retail Row', 
+	'Dusty Depot', 
+	'Fatal Fields', 
+	'Flush Factory', 
+	'Lucky Landing', 
+	'Greasy Grove', 
+	'Haunted Hills', 
+	'Junk Junction', 
+	'Lonely Lodge', 
+	'Loot Lake', 
+	'Moisty Mire', 
+	'Salty Springs', 
+	'Shifty Shafts', 
+	'Snobby Shores', 
+	'Tomato Town', 
+	'Wailing Woods',
+	'Metal Town'
+];
 const button = document.querySelector('button');
 const result = document.querySelector('p.display');
 
@@ -6,9 +27,10 @@ function randomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-button.addEventListener('click', () {
+button.addEventListener('click', () => {
     let n = randomNumber(landingZones.length);
     result.innerHTML = landingZones[n];
-}
+    console.log(landingZones[n]);
+})
 
 
