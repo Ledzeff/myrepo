66e4f60d0251e21acb0f8b971b19aa92c80fe519
Ledzeff <37081231+Ledzeff@users.noreply.github.com>
2018-04-08 20:44:59 -0600

    Update FortniteLZGenerator.js

--- FortniteLZGenerator.js
+++ FortniteLZGenerator.js
@@ -1,33 +1,38 @@
-const checkedLandingZones = [
-	'Anarchy Acres', 
-	'Pleasant Park', 
-	'Tilted Towers', 
-	'Retail Row', 
-	'Dusty Depot', 
-	'Fatal Fields', 
-	'Flush Factory', 
-	'Lucky Landing', 
-	'Greasy Grove', 
-	'Haunted Hills', 
-	'Junk Junction', 
-	'Lonely Lodge', 
-	'Loot Lake', 
-	'Moisty Mire', 
-	'Salty Springs', 
-	'Shifty Shafts', 
-	'Snobby Shores', 
-	'Tomato Town', 
-	'Wailing Woods',
-	'Metal Town'
-];
+window.onload = createCheckboxList(checkedLandingZones);
+
 const uncheckedLandingZones = []; // I realize this isn't necessary in the code, but its practice and could be used in the future
 const landingZones = document.querySelectorAll('input.list');
 const button = document.querySelector('button');
 const result = document.querySelector('p.display');
 
+function createCheckboxList(list) {
+	for ( let i = 0; i < list.length; i++ ) {
+		let li = document.createElement('li');
+		let listItem = document.createElement('input');
+		let ul = document.querySelector('ul.list');
+		listItem.setAttribute('type', 'checkbox');
+		listItem.setAttribute('value', list[i]);
+		listItem.setAttribute('checked', " ");
+		listItem.classList.add('list');
+		li.innerHTML = list[i];
+		li.appendChild(listItem);
+		ul.appendChild(li);
+
+	}
+}
+
 function randomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+function randomColor() {
+    let color = "#";
+	for(let i = 0; i < 6; i++){
+	      color += Math.floor((Math.random() * 16)).toString(16);
+	}
+	return color;
+}
+
 
 button.addEventListener('click', () => {
 	for ( let i = 0; i < landingZones.length; i++) {
@@ -46,7 +51,11 @@ button.addEventListener('click', () => {
 	}
     let n = randomNumber(checkedLandingZones.length);
     result.innerHTML = checkedLandingZones[n];
+    button.style.backgroundColor = randomColor();
     console.log(checkedLandingZones[n]);
 })
 
 console.log(landingZones.length);
+
+
+
