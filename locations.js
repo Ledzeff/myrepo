5b05f6359919cd26856fd79f47ebf5d3cc624ed0
Ledzeff <37081231+Ledzeff@users.noreply.github.com>
2018-04-08 20:45:42 -0600

    Add files via upload

---
+++ locations.js
@@ -0,0 +1,22 @@
+const checkedLandingZones = [
+	'Anarchy Acres', 
+	'Pleasant Park', 
+	'Tilted Towers', 
+	'Retail Row', 
+	'Dusty Depot', 
+	'Fatal Fields', 
+	'Flush Factory', 
+	'Lucky Landing', 
+	'Greasy Grove', 
+	'Haunted Hills', 
+	'Junk Junction', 
+	'Lonely Lodge', 
+	'Loot Lake', 
+	'Moisty Mire', 
+	'Salty Springs', 
+	'Shifty Shafts', 
+	'Snobby Shores', 
+	'Tomato Town', 
+	'Wailing Woods',
+	'Metal Town'
+];
