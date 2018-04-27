c164dc9ef52f8950bf9c13f0c386073ac25bb56c
Ledzeff <37081231+Ledzeff@users.noreply.github.com>
2018-04-26 19:05:01 -0600

    Update FortniteLZGenerator.js

--- FortniteLZGenerator.js
+++ FortniteLZGenerator.js
@@ -1,93 +1,94 @@
+const formList = document.querySelector('form.list');
 window.onload = createCheckboxList(checkedLandingZones);
 
-const landingZones = document.querySelectorAll('input.list');
+const landingZones = [].slice.call(document.querySelectorAll('input.list'));
 const generateBtn = document.querySelector('button.generate');
-const result = document.querySelector('p.display');
+const result = document.querySelector('#display');
 const checkboxBtn = document.querySelector('button.list');
 
 function createCheckboxList(list) {
-	for ( let i = 0; i < list.length; i++ ) {
-		let li = document.createElement('li');
-		let input = document.createElement('input');
-		let ul = document.querySelector('ul.list');
-		input.setAttribute('type', 'checkbox');
-		input.setAttribute('value', list[i]);
-		input.setAttribute('checked', " ");
-		input.classList.add('list');
-		li.innerHTML = list[i];
-		li.appendChild(input);
-		ul.appendChild(li);
-	}
+	list.forEach(item => {
+		const checkbox = document.createElement('input');
+		const label = document.createElement('label');
+		const br = document.createElement('br');
+		checkbox.type = 'checkbox';
+		checkbox.value = item;
+		checkbox.checked = " ";
+		checkbox.id = item;
+		checkbox.className = 'list';
+		label.textContent = item;
+		label.htmlFor = item;
+		formList.appendChild(checkbox);
+		formList.appendChild(label);
+		formList.appendChild(br);
+	});
 }
 
 function randomNumber(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+	return Math.floor(Math.random() * Math.floor(max));
 }
 
 function randomColor() {
-    let color = "#";
+	let color = "#";
 	for(let i = 0; i < 6; i++){
-	      color += Math.floor((Math.random() * 16)).toString(16);
+		color += Math.floor((Math.random() * 16)).toString(16);
 	}
 	return color;
 }
 
 generateBtn.addEventListener('click', () => {
-	for ( let i = 0; i < landingZones.length; i++) {
-		let listItem = landingZones[i]
-		if (listItem.checked === true) {
-			if (checkedLandingZones.indexOf(listItem.value) < 0) {
-				checkedLandingZones.push(listItem.value);
-				uncheckedLandingZones.splice(uncheckedLandingZones.indexOf(listItem.value), 1);
-			} // if listItem is present in checkedLandingZones nothing needs to be done  
-		} else {
-			if (uncheckedLandingZones.indexOf(listItem.value) < 0) {
-				uncheckedLandingZones.push(listItem.value);
-				checkedLandingZones.splice(checkedLandingZones.indexOf(listItem.value), 1);
-			} //if listItem is present in uncheckedLandingZones nothing needs to be done
-		}
-	}
+	let checkedLandingZones = [].slice.call(document.querySelectorAll('input[type=checkbox]:checked'));
 	if (checkedLandingZones.length === 0) {
 		alert('You must check at least 1 box.');
 	} else {
 		let n = randomNumber(checkedLandingZones.length);
-    	result.innerHTML = checkedLandingZones[n];
-    	console.log(checkedLandingZones[n]);
+		result.innerHTML = checkedLandingZones[n].value;
 	}
-    generateBtn.style.backgroundColor = randomColor();
-})
+	generateBtn.style.backgroundColor = randomColor();
+});
 
-for ( let i = 0; i < landingZones.length; i++ ) {
-	landingZones[i].addEventListener('click', () => {
-		let count = 0
-		for ( let i =0; i < landingZones.length; i++ ) {
-			if (landingZones[i].checked === true) {
-				count += 1;
-			} 
-		}
-		if (count === 0) {
-			checkboxBtn.innerHTML = 'Check All Boxes';
-		} else {
-			checkboxBtn.innerHTML = 'Uncheck All Boxes';
-		}
-	})
-}
+formList.addEventListener('click', () => {
+	let count = 0;
+	landingZones.forEach(checkbox => {
+		if (checkbox.checked === true) {
+			count += 1;
+		} 
+	});
+	if (count === 0) {
+		checkboxBtn.innerHTML = 'Check All Boxes';
+	} else {
+		checkboxBtn.innerHTML = 'Uncheck All Boxes';
+	}
+});
 
 checkboxBtn.addEventListener('click', () => {
 	if (checkboxBtn.innerHTML === 'Uncheck All Boxes') {
-		for (let i = 0; i < landingZones.length; i++) {
-			landingZones[i].checked = false
+		landingZones.forEach(landingZone => {
+			landingZone.checked = false;
 			checkboxBtn.innerHTML = 'Check All Boxes';
-		} 
+		});
 	} else {
-		for (let i = 0; i < landingZones.length; i++) {
-		landingZones[i].checked = true
-		checkboxBtn.innerHTML = 'Uncheck All Boxes';
-		}
+		landingZones.forEach(landingZone => {
+			landingZone.checked = true;
+			checkboxBtn.innerHTML = 'Uncheck All Boxes';
+		});
 	}	
-})
+});
 
-console.log(landingZones.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
